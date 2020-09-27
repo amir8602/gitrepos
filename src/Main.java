b2e819839615ad214b2271b7e33678833bcb7d9d
arshiaor <orangashi@yahoo.com>
2020-09-27 20:05:17 +0330

    arshia

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("osheaadasd");
         System.out.println("fsfdfsdfdfs");
         System.out.println("ihhkhkh");
+        System.out.println("arshiaaaa");
     }
 }
